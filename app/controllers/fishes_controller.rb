400e68d21b00adbaeefbafdba193f3ff016cc9f8
summerlin <summerchihuilin@gmail.com>
2019-08-14 15:29:28 +0800

    Fish controller and Fish view:index & show. Also change the routes

--- app/controllers/fishes_controller.rb
+++ app/controllers/fishes_controller.rb
@@ -5,5 +5,7 @@ class FishesController < ApplicationController
 
   def show
     @fish = Fish.find(params[:id])
+    @gamefish = GameFish.new
+    # create a function: one day = one year of fish
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,13 @@ Rails.application.routes.draw do
     resources :questions, only: [:show]
   end
   resources :my_challenges, only: [:index, :show, :create]
-  resources :my_fish, except: [:index, :new, :destroy]
-  resources :fishes, only: [:index, :show]
+
+  resources :fishes, only: [:index, :show] do
+    resources :game_fishes, only: [:create]
+  end
+
+  resources :game_fishes, except: [:destroy, :create, :new]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :challenges, except: [:index, :show]
