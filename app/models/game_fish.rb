5cd9f827b0c00aebf94c471cbbe12a10f6149cba
benedikt kneussl <benediktkneussl@gmail.com>
2019-08-14 13:10:40 +0800

    Devise controller and devise views

--- app/models/game_fish.rb
+++ app/models/game_fish.rb
@@ -3,5 +3,5 @@ class GameFish < ApplicationRecord
   belongs_to :fish
   has_many :game_challenges
   validates :score_happiness, numericality: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
-  validates :user_id, :fish_id, :name, :start_date, :score_health, :score_happiness, :alive, precense: true
+  validates :user_id, :fish_id, :name, :start_date, :score_health, :score_happiness, :alive, presence: true
 end
