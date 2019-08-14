8233c302d4077911790dc65e78d7f5fd325b47e3
benedikt kneussl <benediktkneussl@gmail.com>
2019-08-14 16:28:45 +0800

    FB sign up/in added

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :game_fishes
   has_many :game_challenges, through: :game_fishes
   validates :first_name, :last_name, :country, presence: true
+  devise :omniauthable, omniauth_providers: %i[facebook]
 end
 
 
