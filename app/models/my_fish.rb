04af056096b5ddd2da97e2a3d6023c392be4ee03
anne laws <lawsannechristine@gmail.com>
2019-08-15 15:17:42 +0800

    defining score logic

--- app/models/my_fish.rb
+++ app/models/my_fish.rb
@@ -4,14 +4,26 @@ class MyFish < ApplicationRecord
   belongs_to :fish
   has_many :game_challenges
   validates :score_happiness, numericality: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :score_health, numericality: true, inclusion: { in: (0..100) }
   validates :user_id, :fish_id, :name, :start_date, :score_health, :score_happiness, presence: true
 
-  after_create :update_age
+  after_create :initial_age
 
-  def update_age
+  def initial_age
     new_age = (Date.today - self.created_at.to_date).to_i + 1
     self.age = new_age
-    self.alive = false if self.age > self.fish.max_age
+
     self.save
+  end
+
+  def update_fish_stats
+  end
+
+  def death_probability
+  end
+
+  def dead_or_alive
+  self.alive = false if self.age > self.fish.max_age
+  self.alive = false if self.score_health =< 0
   end
 end
