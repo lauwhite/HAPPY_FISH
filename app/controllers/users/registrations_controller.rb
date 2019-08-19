9c82712089b1da8344131a14e2a70e860793797e
summerlin <summerchihuilin@gmail.com>
2019-08-19 18:13:32 +0800

    add uploading avatar in user sign up page and user edit page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    root to: 'my_fishes#show'
+    'my_fishes#show'
      #'/an/example/path' # Or :prefix_to_your_route  # where do you want to go
   end
 end
