9d6f340ee36bc8e9fe3ec07704b6aaad58b4b4a7
benedikt kneussl <benediktkneussl@gmail.com>
2019-08-21 15:07:09 +0800

    bug solved

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_040630) do
+ActiveRecord::Schema.define(version: 2019_08_21_045653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -194,6 +194,9 @@ ActiveRecord::Schema.define(version: 2019_08_20_040630) do
     t.integer "score"
     t.string "provider"
     t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["level_id"], name: "index_users_on_level_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
