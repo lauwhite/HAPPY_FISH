cdbc8e94225e56b15df27341b7565f99cd9698b6
benedikt kneussl <benediktkneussl@gmail.com>
2019-08-21 15:57:26 +0800

    login and signup tabs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,10 @@
 import "bootstrap";
 import { correctAnswerSearch } from "../quiz/quizAnswers";
+import { bannerLogic } from "../components/banner";
+import { onboardingScoreCalc, onboarding } from "../onboarding-quiz/onboardingQuiz";
 
 correctAnswerSearch();
-import { onboardingScoreCalc, onboarding } from "../onboarding-quiz/onboardingQuiz";
 onboardingScoreCalc();
 onboarding();
+bannerLogic();
+
