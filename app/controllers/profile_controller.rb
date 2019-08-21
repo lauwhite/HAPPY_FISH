4b027c289f9091344005092a5bcfb7d04c530e57
benedikt kneussl <benediktkneussl@gmail.com>
2019-08-21 11:42:43 +0800

    User level shows on profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,5 +2,6 @@ class ProfileController < ApplicationController
   def show
     @user = current_user
     @my_completed_challenges = current_user.game_challenges.where(status: "Completed")
+    @level = Level.find(@user.level_id)
   end
 end
