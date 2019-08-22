cd6b2bd88e46d2d4ce4b35114ab433bc34540964
benedikt kneussl <benediktkneussl@gmail.com>
2019-08-22 16:09:46 +0800

    fb login done and profile pic is up and running

--- db/migrate/20190821045653_add_omniauth_to_users.rb
+++ db/migrate/20190821045653_add_omniauth_to_users.rb
@@ -3,5 +3,7 @@ class AddOmniauthToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :facebook_picture_url, :string
     add_column :users, :token, :string
     add_column :users, :token_expiry, :datetime
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -192,11 +192,11 @@ ActiveRecord::Schema.define(version: 2019_08_21_045653) do
     t.bigint "level_id"
     t.boolean "admin"
     t.integer "score"
-    t.string "provider"
-    t.string "uid"
     t.string "facebook_picture_url"
     t.string "token"
     t.datetime "token_expiry"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["level_id"], name: "index_users_on_level_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
